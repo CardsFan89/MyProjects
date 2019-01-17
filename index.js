dca5e983b124899cf82687629506faff520a4657
Britten Sindelar <brittensindelar@gmail.com>
2019-01-17 16:07:23 -0600

    new css and js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 
+
 alert('Follow us on Twitter and SoundCloud');
 
+var bod = document.querySelector('body');
+
+function doStuff(){
+    bod.className += 'animate';
+}
+
+window.onload = function(){
+    doStuff();
+};
